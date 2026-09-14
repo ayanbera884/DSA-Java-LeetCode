4c44a129c586c37d7d0f2f154874dd79b9af9abc
Ayan Bera <ayanbera884@gmail.com>
2026-09-14 19:45:09 +0530

    Add Max Water Container and organize Single Number

---
+++ 01-Arrays/Maximum SubArray/README.md
@@ -0,0 +1,13 @@
+# Day 38 - Maximum Subarray
+
+## LeetCode 53
+
+### Problem
+
+Given an integer array `nums`, find the contiguous subarray with the largest sum and return its sum.
+
+### Example
+
+**Input:**
+```text
+[-2,1,-3,4,-1,2,1,-5,4]
---
+++ 01-Arrays/maxwater cover/solution.java
@@ -0,0 +1,15 @@
+
+
+public class solution {
+    public int maxWater(int[] height){
+        int left = 0;
+        int right = height.length -1;
+        int maxnumberWater = 0;
+
+        while(left < right){
+            if(height[left] < height[right]){
+                
+            }
+        }
+    }
+}
